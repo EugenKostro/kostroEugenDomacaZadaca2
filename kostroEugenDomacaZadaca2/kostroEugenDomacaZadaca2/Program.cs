a319571e1a741f1be1feffa2cf7397cac6254529
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2019-10-23 22:32:22 +0200

    Add files via upload

---
+++ kostroEugenDomacaZadaca2/kostroEugenDomacaZadaca2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenDomacaZadaca2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            sbyte sto = 100;
+            short tisucu = -1000;
+            char s = "a";
+            float decimalni = 3.123f;
+            float dec = 3.123456789f;
+            bool istina = "true";
+            string rijec = "true";
+            Console.WriteLine("sto= {0}, tisucu= {1}, s= {2}",sto,tisucu,s);
+            Console.WriteLine("decimalni= {0}, dec= {1}, rijec= {2}, istina= {3}", decimalni, dec, rijec, istina);
+
+            Console.ReadKey
+
+        }
+    }
+}
